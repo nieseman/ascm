17fca1e5ce4a0ac460cde539449f1f7a1e7c744d
Manfred Niesert <nieseman@niesert.net>
2020-04-17 13:30:54 +0200

    Restructuring/simplification of UiGtk and MenuFile

--- src/AscmMenuFile.py
+++ src/AscmMenuFile.py
@@ -1,272 +1,120 @@
 #!/usr/bin/python3
 """
-AscmMenuFile.py: Handling of menu file.
+AscmMenuFile.py: Handling of JSON menu file.
 """
 
 import json
-import logging
-import re
 
 from AscmExecCmd import Command
 from AscmInputFile import check_dict, expand_var_list, expand_vars_in_text
 
 
+SEPARATOR_VALUE = "----"
 
-class MenuError(Exception):
-    """ Exception class to be used for errors during reading of menu """
-    pass
-
-
-SEPARATOR_CHAR = "-"
-SEPARATOR_MIN_LEN = 4
 
 class Separator:
+    """
+    A separator in a menu
+    """
+
     def __init__(self, level: int):
         self.level = level
 
 
 class MenuItem:
+    """
+    A named menu entry providing a command
+    """
+
     def __init__(self, level: int, label: str, cmd: Command):
         self.level = level
         self.label = label
         self.cmd = cmd
 
 
-class SubMenu:
+class Menu:
+    """
+    A named foldable (sub-)menu containing items
+    """
+
     def __init__(self, level: int, label: str, items: list):
         self.level = level
         self.label = label
         self.items = items
+        self.unfolded = False
 
+    def flat_iter(self, only_unfolded=False):
+        """
+        Recursively iterate over all menu items
+        """
+        yield self
+        for item in self.items:
+            if isinstance(item, Menu):
+                for subitem in item.flat_iter(only_unfolded):
+                    yield subitem
+            else:
+                 yield item
+            
 
 def convert_dict(level: int, label: str, d: dict, variables: dict):
-    assert isinstance(label, str)
+    """
+    Recursively convert the given dict into a nested menu structure.
 
-    if label == "----":
-        assert d is None
+    Notes:
+    - Along the way, check the proper structure of the dict (which originates
+      from a user-created file).
+    - Order of dict entries is preserved.
+    - As it is a dict, no two menu entries within one submenu should have the
+      same label. (The latter one overwrites the former one.)
+    """
+    assert isinstance(label, str)   # Should be ensured by JSON format.
+
+    # Separator
+    if label == SEPARATOR_VALUE:
+        if d is not None:
+            raise ValueError("dict value of separator must be None")
         return Separator(level)
 
+    # MenuItem (identified by dict key 'cmd')
     elif 'cmd' in d:
-        assert d.keys() in ({'cmd'}, {'cmd', 'opts'})
+        if d.keys() not in ({'cmd'}, {'cmd', 'opts'}):
+            raise ValueError("A command entry may only have keys 'cmd' and 'opts'")
         cmd_str = expand_vars_in_text(d['cmd'], variables)
-        assert isinstance(cmd_str, str)
+        if not isinstance(cmd_str, str):
+            raise ValueError(f"Label '{label}': value of key 'cmd' must be a string.")
+
+        # Determine command options.
         opts = {}
         if 'opts' in d:
             opts_str = d['opts']
-            assert isinstance(opts_str, str)
+            if not isinstance(opts_str, str):
+                raise ValueError(f"Label '{label}': value of key 'opts' must be a string.")
             for flag in opts_str.split(','):
                 opts[flag.strip()] = True
+
         cmd = Command(label, cmd_str, **opts)
         return MenuItem(level, label, cmd)
 
+    # Menu
     else:
         items = [convert_dict(level + 1, key, value, variables)
                                         for key, value in d.items()]
-        return SubMenu(level, label, items)
+        return Menu(level, label, items)
 
 
-class AscmMenuFile:
+def load_menu(file_name: str) -> Menu:
     """
-    Represents the contents of an ascm menu file
-
-    You give it the filename of a menu file, and it provides in self.items
-    a list of MenuItem objects.
+    Load menu data from JSON file, and deliver a nested menu structure.
     """
+    data = json.load(open(file_name))
 
-    def __init__(self, filename, submenu_suffix=""):
-        """
-        Initialize menu from menu file
+    # Check structure of JSON
+    if not isinstance(data, dict):
+        raise ValueError("Top-level element must be a dict.")
+    check_dict(data, 'variables')
+    check_dict(data, 'menu', check_str_values=False)
+    variables = data['variables']
+    menu_data = data['menu']
 
-        Define and initialize all object attributes
-        Construct item list
-
-        Side effects:
-        - object attributes are set
-        - MenuError exceptions may be raised
-        """
-        # Debug output: filename
-        logging.debug("")
-        logging.info(f"Reading menu file: {filename}")
-
-        # Define object attributes.
-        self.filename = filename
-        self.name = "TBD"
-        self.nested_list = self.load_json(filename)
-
-        return
-        self.name, self.flat_list = self.get_flat_list(filename)
-        if not self.flat_list:
-            self.error("Menu file has no menu entries")
-        self.nested_list = self.get_nested_list(self.flat_list)
-
-        for item in self.flat_list:
-            if item.subitems:
-                item.is_submenu = True
-                item.label += submenu_suffix
-       #self.flat_list.clear()
-
-
-    def load_json(self, file_name: str):
-        data = json.load(open(file_name))
-
-        # Check structure of JSON
-        assert isinstance(data, dict), "Top-level element must be a dict."
-        check_dict(data, 'variables')
-        check_dict(data, 'menu', check_str_values=False)
-        variables = data['variables']
-        menu_data = data['menu']
-
-        expand_var_list(variables)
-        return convert_dict(0, "", menu_data, variables)
-
-
-    def error(self, msg, line_num=None):
-        """
-        An error occured while processing the menu, so raise exception.
-        """
-        if line_num is None:
-            loc = f"Menu file {self.filename}"
-        else:
-            loc = f"Menu file {self.filename}, line {line_num}"
-        raise MenuError(f"{loc}: {msg}")
-
-
-    @staticmethod
-    def get_nested_list(flat_list):
-        """
-        TBD
-        """
-
-        # Rebuild item list with correct 'is_submenu' field.
-        # Attribute 'level' has already been checked in get_flat_list().
-        main_items = []
-        last_items = []
-        last_level = 0
-
-        for item in flat_list:
-
-            # If indentataion level is reduced (compared to previous item), skip
-            # finished indentation levels.
-            last_items = last_items[:item.level + 1]
-
-            # Add this item, either to main level of items or to lower level;
-            # and store current item as last item on respective indentation
-            # level.
-            if item.level == 0:
-                main_items.append(item)
-                last_items = [item]
-            else:
-                last_items[item.level - 1].subitems.append(item)
-                if item.level <= last_level:
-                    last_items[item.level] = item
-                else:
-                    last_items.append(item)
-
-            # Remember indentation level.
-            last_level = item.level
-
-        return main_items
-
-
-    def get_flat_list(self, filename):
-        """
-        Set name (first text line in file) and list of MenuItems.
-
-        Side effects:
-        - self.name and self.prel_items are set
-        - MenuError might be raised.
-        - open() and readlines() may raise exceptions.
-        """
-
-        def skip_comments(line):
-            pos = line.find("#")
-            if pos >= 0:
-                line = line[:pos]
-            return line.rstrip()
-
-
-        def get_indent_level(line):
-            indent_width = 4
-            num_spaces = len(line) - len(line.lstrip())
-            if num_spaces % indent_width != 0:
-                self.error("Indentation mismatch", line_num)
-            return num_spaces // indent_width
-
-
-        def split_entry(line):
-            label = line
-            is_separator = (reg.match(line) is not None)
-            cmd = None
-
-            # Separate label and comment in line.
-            if not is_separator:
-                pos = line.find("§§")
-                if pos >= 0:
-                    label = line[:pos]
-                    cmd_str = line[pos + 2:]
-                    if cmd_str and cmd_str[0]:
-                        pos = cmd_str.find(" ")
-                        options = cmd_str[:pos]
-                        cmd_str = cmd_str[pos:]
-                    else:
-                        options = ""
-                    cmd_str = cmd_str.strip()
-
-                    # TBD: Removed later?
-                    opts = {}
-                    attribs_available = {
-                        'w': 'wait',
-                        't': 'term',
-                        'b': 'back',
-                        'r': 'root',
-                    }
-                    for ch, var in attribs_available.items():
-                        if ch in options:
-                            opts[var] = True
-
-                    cmd = Command(label.strip(), cmd_str, **opts)
-
-            return label.strip(), is_separator, cmd
-
-
-        # Main part of get_flat_file().
-        name = None
-        flat_list = []
-
-        # Read menu file
-        line_num = 0
-        old_indent = -1
-        reg = re.compile("^ *%s{%i,}$" % (SEPARATOR_CHAR, SEPARATOR_MIN_LEN))
-        with open(filename, 'r') as fh:
-            for line in fh.readlines():
-                line_num += 1
-
-                # Cut comments, skip empty lines.
-                line = skip_comments(line)
-                if line == "":
-                    continue
-
-                # The first line provides the name of the menu.
-                if name is None:
-                    name = line.strip()
-                    logging.debug(f"Name of menu: {name}")
-                    continue
-
-                # Determine indentation level of line.
-                level = get_indent_level(line)
-                assert level >= 0
-                logging.debug("")
-                logging.debug(f"line {line_num}, indent level = {level}: {line}")
-                if level > old_indent + 1:
-                    self.error("Indentation error", line_num)
-
-                # Check if label is a separator
-                label, is_separator, cmd = split_entry(line)
-
-                # Create new menu item
-                flat_list.append(MenuItem(level, label, cmd, is_separator))
-
-                # Continue loop
-                old_indent = level
-
-        return name, flat_list
+    expand_var_list(variables)
+    return convert_dict(0, "", menu_data, variables)
--- src/AscmUiGtk.py
+++ src/AscmUiGtk.py
@@ -3,26 +3,245 @@
 AscmUiGtk.py: GTK user interface.
 """
 
+import enum
 import os
+from typing import Optional, Union
+
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk as gtk
 gi.require_version("AppIndicator3", "0.1")
 from gi.repository import AppIndicator3 as appindicator
 
-from AscmExecCmd import CommandExecutor
-from AscmMenuFile import AscmMenuFile, MenuError, Separator, MenuItem, SubMenu
+from AscmExecCmd import CommandExecutor, Command
+from AscmMenuFile import Separator, MenuItem, Menu, load_menu
 
 
 
-class AscmUiGtk():
+# Menu items for generic tasks (i.e. tasks not defined in the menu file).
+GenericMenuItem = enum.Enum('GenericMenuItem', """
+    toggle_cmd_window
+    edit_menu_file
+    load_menu_file
+    main_quit
+""")
+
+
+
+class AscmTrayIndicator:
+    """
+    Tray indicator for ascm menu.
+    Can be populated with items from menu file.
+    Also provides generic menu items.
+
+    Main front-end function:
+    - populate()
+    """
+
+    ICON_DEFAULT = "ascm_play_solid.svg"    # TBD
+
+    def __init__(self, ui: 'AscmUiGtk', icon: Optional[str]):
+        self.ui = ui
+        if icon is None:
+            icon = self.ICON_DEFAULT
+
+        # Create and store tray indicator.
+        appindicator_id = 'ascm_indicator'
+        indicator = appindicator.Indicator.new(
+            appindicator_id,
+            os.path.abspath(icon),
+            appindicator.IndicatorCategory.SYSTEM_SERVICES)
+        tray_menu = gtk.Menu()
+        indicator.set_menu(tray_menu)
+        indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+
+        self.indicator = indicator
+
+
+    def populate(self, menu):
+        """
+        Populate tray menu with all items in given menu file.
+        """
+
+        # Clear all menu items.
+        tray_menu = self.indicator.get_menu()
+        for item in tray_menu.get_children():
+            tray_menu.remove(item)
+
+        # Create new menu items.
+        for item in menu.items:
+            self.add_items_to_tray(tray_menu, item)
+        self.add_generic_items_to_tray_menu()
+
+        tray_menu.show_all()
+
+
+    def add_menu_item(self, menu, event, label):
+        """
+        Add given event under given label into given menu.
+        """
+        action = self.ui.get_callback(event)
+        item = gtk.MenuItem(label)
+        item.connect('activate', action)
+        menu.append(item)
+
+
+    def add_items_to_tray(self, menu, item):
+        """
+        Recursively add all ascm menu items to GTK tray menu.
+        """
+        if isinstance(item, Separator):
+            menu.append(gtk.SeparatorMenuItem())
+
+        elif isinstance(item, MenuItem) and item.cmd.cmd_str:
+            self.add_menu_item(menu, item.cmd, item.label)
+
+        else: # isinstance(item, Menu)
+            submenu = gtk.Menu()
+            menu_item = gtk.MenuItem(item.label)
+            menu_item.set_submenu(submenu)
+            menu.append(menu_item)
+            for item in item.items:
+                self.add_items_to_tray(submenu, item)
+
+
+    def add_generic_items_to_tray_menu(self):
+        """
+        Add additional menu with generic items to (already created) tray menu.
+        """
+        menu = gtk.Menu()
+
+        self.add_menu_item(menu, GenericMenuItem.toggle_cmd_window, 'Command window')
+        self.add_menu_item(menu, GenericMenuItem.edit_menu_file,    'Edit menu file')
+        self.add_menu_item(menu, GenericMenuItem.load_menu_file,    'Reload menu file')
+        self.add_menu_item(menu, GenericMenuItem.main_quit,         'Quit ascm')
+        generic_item = gtk.MenuItem('ascm')
+        generic_item.set_submenu(menu)
+
+        tray_menu = self.indicator.get_menu()
+        tray_menu.append(gtk.SeparatorMenuItem())
+        tray_menu.append(generic_item)
+
+
+
+class AscmCmdWindow:
+    """
+    A command window which provides all menu items in a foldable list.
+
+    Main front-end functions:
+    - populate()
+    - toggle_visibility()
+    """
+
+    def __init__(self, ui: 'AscmUiGtk'):
+        self.ui = ui
+
+        store = gtk.TreeStore(str, int)     # Label
+        column = gtk.TreeViewColumn("Task", gtk.CellRendererText(), text=0)
+
+        view = gtk.TreeView(model=store)
+        view.append_column(column)
+        view.set_headers_visible(False)
+
+        scroll = gtk.ScrolledWindow()
+        scroll.set_vexpand(True)
+        scroll.add(view)
+
+        button_exec = gtk.Button("Execute")
+        button_exec.connect('clicked', self.btn_execute)
+
+        window = gtk.Dialog(parent=self.ui.hidden_main_window, title="ascm")
+        window.set_default_size(300, 400)
+        window.connect('delete-event',
+                self.ui.get_callback(GenericMenuItem.toggle_cmd_window))
+
+        box = window.get_content_area()
+        box.add(scroll)
+        box.add(button_exec)
+
+        self.cmd_window = window
+        self.tree_view = view
+        self.tree_store = store
+
+
+    def populate(self, menu):
+        """
+        Populate tree list in command window with all items in given menu file.
+        """
+
+        # It seems that in Gtk, items in a list need to be identified by a
+        # 32-bit integer index. Therefore, use helper mappings between unique
+        # integers and the commands of MenuItem objects menu items.
+        self.idx_to_cmd = {}
+        self.cmd_to_idx = {}
+        for idx, item in enumerate(menu.flat_iter()):
+            if isinstance(item, MenuItem):
+                cmd = item.cmd
+                self.idx_to_cmd[idx] = cmd
+                self.cmd_to_idx[cmd] = idx
+
+        # Create new menu items.
+        self.items_stack = [None]
+        for item in menu.items:
+            self.add_items_to_tree(item)
+
+        del self.cmd_to_idx, self.items_stack
+        # cmd_to_idx was only used for construction of list.
+        # idx_to_cmd is still needed in btn_execute().
+
+        # Show widgets.
+        self.cmd_window.show_all()
+        self.cmd_window.set_visible(False)
+
+
+    def add_items_to_tree(self, item):
+        """
+        Recursively add item and all subitems to the tree list.
+        """
+        if isinstance(item, Separator):
+            return
+
+        if isinstance(item, MenuItem):
+            idx = self.cmd_to_idx[item.cmd]
+        else:
+            idx = 0
+        last_cmd_win_item = self.tree_store.append(self.items_stack[-1], (item.label, idx))
+        self.items_stack.append(last_cmd_win_item)
+        if isinstance(item, Menu):
+            for item in item.items:
+                self.add_items_to_tree(item)
+        self.items_stack.pop()
+
+
+    def btn_execute(self, _button):
+        """
+        Called by button 'Execute' in command window.
+        """
+        (model, iter) = self.tree_view.get_selection().get_selected()
+        idx = model[iter][1]
+        if idx > 0:
+            cmd = self.idx_to_cmd[idx]
+            self.ui.callback(cmd)
+
+
+    def toggle_visibility(self):
+        """
+        Toggle window between being (non-)visible.
+        """
+        self.cmd_window.set_visible(not self.cmd_window.is_visible())
+
+
+
+class AscmUiGtk:
     """
     A GTK-based user interface for ascm
 
     It provides a tray icon with command menu as well as a command window.
-    """
 
-    ICON_DEFAULT = "ascm_play_solid.svg"    # TBD
+    Main front-end functions:
+    - run()
+    - finish()
+    """
 
     def __init__(self, args):
         """
@@ -30,141 +249,22 @@ class AscmUiGtk():
         """
         self.args = args
         self.cmd_executor = CommandExecutor(True, args.pkexec)
-        self.indicator = self.create_tray_icon()
-        self.cmd_window, self.tree_view, self.tree_store = self.create_cmd_window()
 
-        self.menu_file = None
-        self.load_menu_file(False)
+        self.hidden_main_window = gtk.Window()
+        self.cmd_window_ = AscmCmdWindow(self)
+        self.tray_indicator = AscmTrayIndicator(self, args.icon)
 
-
-    def create_tray_icon(self):
-        """
-        TBD
-        """
-        if self.args.icon is None:
-            icon_path = self.ICON_DEFAULT
-        else:
-            icon_path = self.args.icon
-        icon_path = os.path.abspath(icon_path)
-        appindicator_id = 'ascm_indicator'
-        indicator = appindicator.Indicator.new(
-            appindicator_id,
-            icon_path,
-            appindicator.IndicatorCategory.SYSTEM_SERVICES)
-        tray_menu = gtk.Menu()
-        indicator.set_menu(tray_menu)
-        indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
-
-        return indicator
+        self.load_menu_file()
 
 
-    def create_cmd_window(self):
-        """
-        TBD
-        """
-        self._hidden_main_window = gtk.Window()
-
-        store = gtk.TreeStore(str, int)     # Label
-        column = gtk.TreeViewColumn("Task", gtk.CellRendererText(), text=0)
-
-        treeview = gtk.TreeView(model=store)
-        treeview.append_column(column)
-        treeview.set_headers_visible(False)
-
-        scroll = gtk.ScrolledWindow()
-        scroll.set_vexpand(True)
-        scroll.add(treeview)
-
-        button_exec = gtk.Button("Execute")
-        button_exec.connect('clicked', self.run_cmd_from_window)
-
-        cmd_window = gtk.Dialog(parent=self._hidden_main_window, title="ascm")
-        cmd_window.set_default_size(300, 400)
-        cmd_window.connect('delete-event', self.hide_cmd_window)
-
-        box = cmd_window.get_content_area()
-        box.add(scroll)
-        box.add(button_exec)
-
-        return cmd_window, treeview, store
-
-
-    def add_items_to_tray(self, menu, item):
-        """
-        TBD
-        """
-        if isinstance(item, Separator):
-            menu.append(gtk.SeparatorMenuItem())
-        else:
-            menu_item = gtk.MenuItem(item.label)
-            menu.append(menu_item)
-            if isinstance(item, MenuItem) and item.cmd.cmd_str:
-                self.item_map[menu_item] = item
-                menu_item.connect('activate', self.run_cmd_from_tray)
-            if isinstance(item, SubMenu):
-                submenu = gtk.Menu()
-                menu_item.set_submenu(submenu)
-                for item in item.items:
-                    self.add_items_to_tray(submenu, item)
-
-
-    def add_items_to_cmd_window(self, item):
-        """
-        TBD
-        """
-        if isinstance(item, Separator):
-            return
-
-        self._idx += 1
-        if isinstance(item, MenuItem):
-            cmd_win_item = item.label, self._idx
-        else:
-            cmd_win_item = item.label, 0
-        last_cmd_win_item = self.tree_store.append(
-            self._items_stack[-1], cmd_win_item)
-        self._items_stack.append(last_cmd_win_item)
-        if isinstance(item, MenuItem):
-            self.item_map[self._idx] = item
-        if isinstance(item, SubMenu):
-            for item in item.items:
-                self.add_items_to_cmd_window(item)
-        self._items_stack.pop()
-
-
-    def add_generic_items_to_tray_menu(self):
-        """
-        Add generic menu items to (already created) tray menu
-        """
-
-        def add_menu_item(action, label):
-            """
-            TBD
-            """
-            item = gtk.MenuItem(label)
-            item.connect('activate', action)
-            submenu.append(item)
-
-        submenu = gtk.Menu()
-        add_menu_item(self.toggle_cmd_window, 'Command window')
-        add_menu_item(self.edit_menu_file,    'Edit menu file')
-        add_menu_item(self.load_menu_file,    'Reload menu file')
-        add_menu_item(gtk.main_quit,          'Quit ascm')
-        generic_item = gtk.MenuItem('ascm')
-        generic_item.set_submenu(submenu)
-
-        tray_menu = self.indicator.get_menu()
-        tray_menu.append(gtk.SeparatorMenuItem())
-        tray_menu.append(generic_item)
-
-
-    def load_menu_file(self, _dummy):
+    def load_menu_file(self):
         """
         Load menu file; create & populate tray menu and command window.
         """
 
-        # Load menu file
+        # Load menu file.
         try:
-            menu_file = AscmMenuFile(self.args.menu_file)
+            menu = load_menu(self.args.menu_file)
         except MenuError as e:
             err_msg = f"Cannot process menu file {self.args.menu_file}.\n" + \
                       f"Reason: {e}"
@@ -176,70 +276,45 @@ class AscmUiGtk():
                 message_format=err_msg).run()
             return
 
-        self.menu_file = menu_file
-
-        # Clear all menu items.
-        tray_menu = self.indicator.get_menu()
-        for item in tray_menu.get_children():
-            tray_menu.remove(item)
-
-        # Create new menu items.
-        self.item_map = {}
-        self._items_stack = [None]
-        self._idx = 0
-        for item in self.menu_file.nested_list.items:
-            self.add_items_to_tray(tray_menu, item)
-            self.add_items_to_cmd_window(item)
-        self.add_generic_items_to_tray_menu()
-
-        # Show widgets.
-        tray_menu.show_all()
-        self.cmd_window.show_all()
-        self.cmd_window.set_visible(False)
+        # Update tray and command window.
+        self.cmd_window_.populate(menu)
+        self.tray_indicator.populate(menu)
 
 
-    def run_cmd_from_tray(self, menu_item):
+    def get_callback(self, event):
         """
-        Called by selection of menu item in system tray menu.
+        Obtain a closure to call callback() with given event.
         """
-        assert menu_item in self.item_map
-        cmd = self.item_map[menu_item].cmd
-        self.cmd_executor.run(cmd)
+
+        def get_callback_(*_):
+            self.callback(event)
+
+        return get_callback_
 
 
-    def run_cmd_from_window(self, _button):
+    def callback(self, event: Union[Command, GenericMenuItem]):
         """
-        Called by button 'Execute' in command window.
+        This function is called when a menu item is selected
+        (in the tray menu or in the command window).
         """
-        # TBD: rename iter
-        (model, iter) = self.tree_view.get_selection().get_selected()
-        cmd_idx = model[iter][1]
-        if cmd_idx > 0:
-            cmd = self.item_map[cmd_idx].cmd
-            self.cmd_executor.run(cmd)
+        if isinstance(event, Command):
+            self.cmd_executor.run(event)
 
+        elif isinstance(event, GenericMenuItem):
+            if event == GenericMenuItem.toggle_cmd_window:
+                self.cmd_window_.toggle_visibility()
 
-    # TBD: Is this order of arguments correct?
-    def hide_cmd_window(self, _widget=None, *_data):
-        """
-        Hide command window; activated by close button of command window
-        """
-        self.cmd_window.set_visible(False)
-        return True
+            elif event == GenericMenuItem.edit_menu_file:
+                self.cmd_executor.edit(self.args.menu_file)
 
+            elif event == GenericMenuItem.load_menu_file:
+                self.load_menu_file()
 
-    def toggle_cmd_window(self, _dummy):
-        """
-        Show/hide the command window.
-        """
-        self.cmd_window.set_visible(not self.cmd_window.is_visible())
+            elif event == GenericMenuItem.main_quit:
+                gtk.main_quit()
 
-
-    def edit_menu_file(self, _dummy):
-        """
-        Edit the menu file in an external editor.
-        """
-        self.cmd_executor.edit(self.args.menu_file)
+        else:
+            assert False
 
 
     @staticmethod
